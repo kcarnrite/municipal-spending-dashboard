84b42cc80d3f0fce8489081ec177dcb5eac47dc0
kcarnrite <kfcarnrite@gmail.com>
2025-01-28 13:05:29 -0500

    Adding super basic functions to get super simple live data loading

---
+++ backend/src/index.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+import data
+
+app = Flask(__name__)
+#CORS(app, resources={'*': {'origins': 'localhost:5173'}})
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/api/category/<category>", methods=["GET"])
+def get_by_category(category):
+    match category:
+        case 'governance':
+            return_object = data.get_governance_data()
+            return return_object
+        case _:
+            return Respo
