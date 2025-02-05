9bf82fa703a3b7adc53524777a9d104c8b4d13ac
kcarnrite <kfcarnrite@gmail.com>
2025-02-05 11:47:49 -0500

    Adding loading and error logic

--- dashboard/src/Table.jsx
+++ dashboard/src/Table.jsx
@@ -1,32 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
+
+function dataFetchReducer(state, action) {
+    switch (action.type){
+        case 'FETCH_INIT':
+            return {...state, isLoading:true};
+        
+        case 'FETCH_FAIL':
+            return {...state, isLoading:false, isError:true};
+        
+        case 'FETCH_SUCCESS':
+            return {...state, isLoading:false, isError:false, data:action.payload};
+        default:
+            new Error()
+        
+    }
+}
 
 function Table({headers, table_content, filters}) {
     const APIBASEURL = 'http://localhost:5000/api/'
-    const [tableData, setTableData] = useState([])
+    const [table, dispatchTable] = useReducer(dataFetchReducer,{data: [], isLoading: false, isError: false})
     const [sortBy, setSortBy] = useState(null);
     const [descending, setDescending] = useState(false);
     useEffect(() => {
+        dispatchTable({type:"FETCH_INIT"})
         fetch(APIBASEURL + 'category/' + filters.query)
         .then(response => response.json())
         .then(data => {
-            setTableData(data);
-        })
+            dispatchTable({type: 'FETCH_SUCCESS', payload: data});
+        }).catch(() => dispatchTable({type: 'FETCH_FAIL'}))
     }, [filters.query])
 
     //TODO: Refactor filtering?
-    var tableItems = tableData;
+    var tableItems = table;
     if(!filters.ST) {
-        tableItems = tableItems.filter(item => (
+        tableItems = table.data.filter(item => (
         item[2] != "ST"
         ));
     }
     if(!filters.LT) {
-        tableItems = tableItems.filter(item => (
+        tableItems = table.data.filter(item => (
             item[2] != "LT"
         ));
     }
     if(!filters.UT) {
-        tableItems = tableItems.filter(item => (
+        tableItems = table.data.filter(item => (
             item[2] != "UT"
         ));
     }
@@ -42,7 +59,7 @@ function Table({headers, table_content, filters}) {
     }
 
     if(sortBy != null) {
-        tableItems = tableItems.sort((a,b) => {
+        tableItems = table.data.sort((a,b) => {
         if(a[sortBy] === b[sortBy]) {
             return 0;
         }
@@ -66,28 +83,36 @@ function Table({headers, table_content, filters}) {
 
     })
     }
-    
-    return (
-        <table className="border-collapse">
-            <thead>
-                <tr className="border-2 border-black bg-slate-200">
-                    {headers.map((label, idx) => (
-                        <th className="border-2 border-black" key={idx} onClick={() => handleSort(idx)}>
-                            {label + (sortBy == idx ? (descending ? ' \u2191' : ' \u2193') : "")}
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                    {tableItems.map((item, idx) => (
-                        <tr className="border-2 border-black" key={idx}>
-                                <Item data={item} key={idx} />
-                        </tr>
-                    ))}
-            </tbody>
-        </table>
-    )
-}
+        if(table.isError) {
+            return <p>Error: Something went wrong...</p>
+        }
+        else if(table.isLoading) {
+            return <img src="/spinner-100.png" className="w-[30px] animate-spin"></img>
+        }
+        else {
+            return (
+            <table className="border-collapse">
+                <thead>
+                    <tr className="border-2 border-black bg-slate-200">
+                        {headers.map((label, idx) => (
+                            <th className="border-2 border-black" key={idx} onClick={() => handleSort(idx)}>
+                                {label + (sortBy == idx ? (descending ? ' \u2191' : ' \u2193') : "")}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                        {table.data.map((item, idx) => (
+                            <tr className="border-2 border-black" key={idx}>
+                                    <Item data={item} key={idx} />
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+       
+            )
+        }
+    }
 
 function Item({data}) {
     return (
