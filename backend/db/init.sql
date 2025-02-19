3fc2316862323444330701fe45c8d9b4a3291b54
kcarnrite <kfcarnrite@gmail.com>
2025-02-18 23:50:47 -0500

    Adding init file for DB layout

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -4,31 +4,34 @@ USE MunicipalDashboard;
 
 CREATE TABLE Municipalities(
     id SMALLINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(25),
+    name VARCHAR(50),
     population INT,
     tier CHAR(2),
     PRIMARY KEY (id));
 
 CREATE TABLE Categories(
 	line_number SMALLINT NOT NULL,
-	category VARCHAR(50) NOT NULL,
+	category VARCHAR(100) NOT NULL,
 	PRIMARY KEY (line_number));
 
 
 CREATE TABLE ExpenseData(
 id SMALLINT NOT NULL AUTO_INCREMENT,
 c_id SMALLINT NOT NULL,
+m_id SMALLINT NOT NULL,
 `year` YEAR NOT NULL,
-wages INT NOT NULL,
-interest INT NOT NULL,
-materials INT NOT NULL,
-contracted_services INT NOT NULL,
-rents_fin_expenses INT NOT NULL,
-external_transfers INT NOT NULL,
-total_before_adjustments INT NOT NULL,
-total_expenses INT NOT NULL,
-adjustments INT NOT NULL,
-program_support INT NOT NULL,
+wages BIGINT NOT NULL,
+interest BIGINT NOT NULL,
+materials BIGINT NOT NULL,
+contracted_services BIGINT NOT NULL,
+rents_fin_expenses BIGINT NOT NULL,
+external_transfers BIGINT NOT NULL,
+amortization BIGINT NOT NULL,
+total_before_adjustments BIGINT NOT NULL,
+total_expenses BIGINT NOT NULL,
+adjustments BIGINT NOT NULL,
+program_support BIGINT NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (c_id) REFERENCES Categories (line_number)
+FOREIGN KEY (c_id) REFERENCES Categories (line_number),
+FOREIGN KEY (m_id) REFERENCES Municipalities (id)
 );
