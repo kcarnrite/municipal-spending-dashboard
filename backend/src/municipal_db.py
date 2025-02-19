3302d8dbbf42b75aa69cead841cb74ac7c69d6b4
kcarnrite <kfcarnrite@gmail.com>
2025-02-19 15:06:50 -0500

    Refactored functions to reduce repeated logic

--- backend/src/municipal_db.py
+++ backend/src/municipal_db.py
@@ -5,23 +5,23 @@ def get_connection():
     connection = db.connect(user=keys.DB_USER, password=keys.DB_PASSWORD,host="localhost", port=keys.DB_PORT)
     return connection
 
-def get_total_data(category):
+def get_data(query):
     connection = get_connection()
     cursor = connection.cursor()
     connection.database = "MunicipalDashboard"
-    cursor.execute("SELECT name, total_expenses, m.tier FROM ExpenseData INNER JOIN Categories ON c_id = line_number INNER JOIN Municipalities m ON m_id = m.id WHERE category = %s", (category,))
+    cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
     connection.close()
     return data
 
+def get_total_data(category):
+    query = "SELECT name, total_expenses, m.tier FROM ExpenseData INNER JOIN Categories ON c_id = line_number INNER JOIN Municipalities m ON m_id = m.id WHERE category = %s", (category,)
+    data = get_data(query)
+    return data
+
 def get_by_capita_data(category):
-    connection = get_connection()
-    cursor = connection.cursor()
-    connection.database = "MunicipalDashboard"
-    cursor.execute("SELECT name, total_expenses, m.population, m.tier FROM ExpenseData INNER JOIN Categories ON c_id = line_number INNER JOIN Municipalities m ON m_id = m.id WHERE category = %s AND population IS NOT NULL", (category,))
-    data = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    query = "SELECT name, total_expenses, m.population, m.tier FROM ExpenseData INNER JOIN Categories ON c_id = line_number INNER JOIN Municipalities m ON m_id = m.id WHERE category = %s AND population IS NOT NULL", (category,)
+    data = get_data(query)
     return data
 
