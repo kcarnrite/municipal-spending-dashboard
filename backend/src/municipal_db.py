10307a7615ccf3398ae1d243e23aa8446f4fce2f
kcarnrite <kfcarnrite@gmail.com>
2025-02-19 15:03:40 -0500

    Adding functions to query data from db

---
+++ backend/src/municipal_db.py
@@ -0,0 +1,27 @@
+import mysql.connector as db
+import constants as keys
+
+def get_connection():
+    connection = db.connect(user=keys.DB_USER, password=keys.DB_PASSWORD,host="localhost", port=keys.DB_PORT)
+    return connection
+
+def get_total_data(category):
+    connection = get_connection()
+    cursor = connection.cursor()
+    connection.database = "MunicipalDashboard"
+    cursor.execute("SELECT name, total_expenses, m.tier FROM ExpenseData INNER JOIN Categories ON c_id = line_number INNER JOIN Municipalities m ON m_id = m.id WHERE category = %s", (category,))
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return data
+
+def get_by_capita_data(category):
+    connection = get_connection()
+    cursor = connection.cursor()
+    connection.database = "MunicipalDashboard"
+    cursor.execute("SELECT name, total_expenses, m.population, m.tier FROM ExpenseData INNER JOIN Categories ON c_id = line_number INNER JOIN Municipalities m ON m_id = m.id WHERE category = %s AND population IS NOT NULL", (category,))
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return data
+
