4c7ff1b860479b7ec37f8da070d24fa721c1c134
kcarnrite <kfcarnrite@gmail.com>
2025-02-20 15:12:17 -0500

    Combine all logic for filter controls

--- dashboard/src/DropdownFilter.jsx
+++ dashboard/src/DropdownFilter.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 function DropdownFilter({options, currentValue, onSelection}) {
     return (
-        <select value={currentValue} className="drop-shadow-md" onChange={onSelection}>
-            {options.map(item => (
-                <option key={item.internalValue} value={item.internalValue}>{item.displayValue}</option>
+        <select defaultValue={currentValue} value={currentValue} className="drop-shadow-md" onChange={onSelection}>
+            {options.map((item,idx) => (
+                <option key={idx} value={item.internalValue}>{item.displayValue}</option>
             ))}
         </select>
     );
--- dashboard/src/FilterControls.jsx
+++ dashboard/src/FilterControls.jsx
@@ -1,17 +1,34 @@
 import DropdownFilter from "./DropdownFilter";
+import CheckBoxModule from "./CheckBoxModule"
 
 
 function FilterControls({filterState, onFilterChange}) {
     return (
-        <div>
+        <div className="mx-30 flex-nowrap flex gap-8">
             <DropdownFilter 
                 options={
-                    [{internalValue:"perCapita", displayValue:"By Capita"},
-                    {internalValue:"total", displayValue:"Total"}
+                    [{internalValue:"total", displayValue:"Total"},
+                    {internalValue:"perCapita", displayValue:"By Capita"}
                     ]}
                 onSelection={(event) => onFilterChange({type:'CHANGE_MEASUREMENT', payload:event.target.value})}
                 currentValue={filterState.measurementValue}
             />
+
+            <DropdownFilter
+                options={
+                    [
+                        {displayValue: "Governance", internalValue: "governance"},
+                        {displayValue: "Fire", internalValue: "fire"},
+                        {displayValue: "Police", internalValue: "police"},
+                        {displayValue: "Paved Roads", internalValue: "pavedRoads"},
+                        {displayValue: "Unpaved Roads", internalValue: "unpavedRoads"},
+                    ]
+                }
+                onSelection={(event) => onFilterChange({type: 'CHANGE_QUERY', payload:event.target.value})}
+                currentValue={filterState.query}
+            />
+
+            <CheckBoxModule moduleState={filterState} filterReducer={onFilterChange} />
         </div>
     );
 
