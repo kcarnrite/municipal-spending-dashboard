13baf4f04f908dee5843539dff68f4051965caf1
kcarnrite <kfcarnrite@gmail.com>
2025-02-26 14:40:53 -0500

    Remove defaultValue to remove browser warning

--- dashboard/src/DropdownFilter.jsx
+++ dashboard/src/DropdownFilter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function DropdownFilter({options, currentValue, onSelection}) {
     return (
-        <select defaultValue={currentValue} value={currentValue} className="drop-shadow-md" onChange={onSelection}>
+        <select value={currentValue} className="drop-shadow-md" onChange={onSelection}>
             {options.map((item,idx) => (
                 <option key={idx} value={item.internalValue}>{item.displayValue}</option>
             ))}
