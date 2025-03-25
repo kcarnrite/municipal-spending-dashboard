221c27096657aff7bb71b09eee7e9f29fb1a309f
kcarnrite <kfcarnrite@gmail.com>
2025-03-25 16:41:01 -0400

    Dropdown functionality done - may need more styling to prevent layout changes

--- dashboard/src/Dropdown.jsx
+++ dashboard/src/Dropdown.jsx
@@ -22,34 +22,62 @@ function Dropdown({ text, children, className}) {
     }, [listActive])
     const handleClick = (event) => {
         setListActive(true)
-        if(event.target.dataset.type == 'item') {
-            setListActive(false)
+        if(event.target.dataset.type != 'sublist') {
+            setListActive(!listActive)
         }
     }
     return (
-        <div onClick={handleClick} className={className} ref={dropdownRef}>
-            <p className="cursor-default">{text}</p>
-            {listActive ? children : null}
+        <div onClick={handleClick} className={"bg-white rounded-md shadow-md" } ref={dropdownRef}>
+            <div className="flex flex-row select-none rounded-md items-center justify-items-start gap-4 px-4 hover:bg-gray-300">
+                <p >{children ? text : 'Loading...'}</p>
+                <p className="text-xs justify-self-end">▼</p>
+            </div>
+            <div className="absolute rounded-md shadow-lg">
+                {listActive ? children : null}
+            </div>
         </div>
     )
 }
 
 function SubList({ text, children }) {
     const [listActive, setListActive] = useState(false)
+        var dropdownRef = useRef(null)
+        useEffect(() => {
+        function handleClickOutside() {
+            if(dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                console.log("HANDLE CLICK ACTIVATED")
+                setListActive(false)
+            }
+        }
+
+        if(listActive) {
+            document.addEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [listActive])
     const handleClick = () => {
         setListActive(!listActive)
     }
     return (
-        <>
-            <p data-type="sublist" onClick={handleClick}>{text}</p>
-            {listActive ? children : null}
-        </>
+        <div onClick={handleClick} data-type="sublist" className="flex flex-row select-none px-6 bg-gray-100 hover:bg-gray-300" ref={dropdownRef}>
+            <p data-type="sublist">{text}</p>
+            <div className="flex flex-col absolute left-full shadow-lg w-fit ">
+                {listActive ? children : null}
+            </div>
+        </div>
     )
 }
 
 function ListItem({ text, onClick}) {
     return (
-        <p data-type="item" onClick={onClick}>{text}</p>
+        <div>
+            <p data-type="item" className="select-none px-6 hover:bg-gray-300 bg-gray-100 md:text-nowrap"onClick={onClick}>
+                {text}
+            </p>
+        </div>
     );
 }
 
